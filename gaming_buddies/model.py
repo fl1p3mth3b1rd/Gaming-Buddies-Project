078056dd1b07e80f29c2b1241b2644b0ad6dc71c
Artur <unitedfan@list.ru>
2021-06-18 04:27:03 +0700

    Добавили модель Post_information и сделали миграцию в БД

--- gaming_buddies/model.py
+++ gaming_buddies/model.py
@@ -11,7 +11,20 @@ class User_general_information(db.Model):
     second_name = db.Column(db.String(100))
     birth_date = db.Column(db.Date)
     gender = db.Column(db.String(20))
+    posts = db.relationship('Post_information', backref='user')
     # profile_photo = 
 
     def __repr__(self):
         return "<User id: {}, User nickname {}>".format(self.id, self.nickname)
+
+class Post_information(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user_general_information.id'))
+    post_id = db.Column(db.Integer, primary_key=True)
+    # game_id = 
+    country = db.Column(db.String(100))
+    timezone = db.Column(db.String(50))
+    description_as_author = db.Column(db.Text)
+    purpose_as_author = db.Column(db.Text)
+    discord = db.Column(db.String(50))
+    prime_time = db.Column(db.String(50))
+    post_status = db.Column(db.String(50))
